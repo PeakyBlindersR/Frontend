29e490b6a07bdf53b00f5c934bc44a6cb9a0ceaf
victorinais <vicmarin50@gmail.com>
2024-05-06 21:44:30 -0500

    Intento de buscador

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function saveNoteId(id) {
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
-
+let Notas = [];
 //Hacer una petición mediante el metodo fetch a la base de datos luego pintarlas en el html por medio del template de js.
 template_index = "";
 fetch("http://localhost:5219/api/Notas")
@@ -15,26 +15,21 @@ fetch("http://localhost:5219/api/Notas")
     console.log(data);
     if (Array.isArray(data)) {
       data.forEach((element) => {
-        let title = element.title
-        let content = element.content
-        let date = element.date
-        let id = element.id
-
         template_index += `
             <div class="col-md-3 col-sm-6 col-lg-3">
                 <div class="card">
                     <div class="card-header">
-                    <h4 class="card-title text-uppercase fw-bold">${title}</h4>
+                    <h4 class="card-title text-uppercase fw-bold">${element.title}</h4>
                     </div>
                     <div class="card-body">
-                        <p class="card-text">${content}</p>
+                        <p class="card-text">${element.content}</p>
                     </div>
                     <div class="card-footer">
-                        <p>${date}</p>
+                        <p>${element.date}</p>
                     </div>
                     <div class="d-flex justify-content-center gap-2 mt-2">
-                        <a href="./Actualizar.html" class="btn btn-primary mb-2" onclick="saveNoteId(${id})">Editar</a>
-                        <button class="btn btn-danger mb-2" onclick="eliminarNota(${id})">Eliminar</button>
+                        <a href="./Actualizar.html" class="btn btn-primary mb-2">Editar</a>
+                        <button class="btn btn-danger mb-2" onclick="eliminarNota(${element.id})">Eliminar</button>
                     </div>
                 </div>
             </div>
@@ -47,7 +42,7 @@ fetch("http://localhost:5219/api/Notas")
     nota.innerHTML = template_index;
   });
 
-  //---------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------
 
 //Hacer una petición mediante el metodo fetch para crear notas en la base de datos
 function crearNota() {
@@ -100,44 +95,84 @@ function eliminarNota(id) {
 //-----------------------------------------------------------------------------------------------------------------------------------------
 
 //Hacer una petición fetch para buscar en la base de datos
-/* function buscarNota() {
-  template_notas = "";
+/* let searchTerm = document.getElementById("search").value;
+fetch(`http://localhost:5219/api/Notas/${searchTerm}`)
+.then((r) => r.json())
+.then((data) => {
+  let resultados = data.filter(nota => nota.title.toLowerCase().includes(searchTerm.toLowerCase()) || nota.content.toLowerCase().includes(searchTerm.toLowerCase()));
+  console.log(resultados);
+  if (Array.isArray(data)) {
+    data.forEach((element) => {
+      let title = element.title
+      let content = element.content
+      let date = element.date
+      let id = element.id
 
-  // Assuming "nota" is the ID of the element where you render notes
-  const nota = document.getElementById("nota"); // Get the element reference
+      template_index += `
+          <div class="col-md-3 col-sm-6 col-lg-3">
+              <div class="card">
+                  <div class="card-header">
+                  <h4 class="card-title text-uppercase fw-bold">${title}</h4>
+                  </div>
+                  <div class="card-body">
+                      <p class="card-text">${content}</p>
+                  </div>
+                  <div class="card-footer">
+                      <p>${date}</p>
+                  </div>
+                  <div class="d-flex justify-content-center gap-2 mt-2">
+                      <a href="./Actualizar.html" class="btn btn-primary mb-2" onclick="saveNoteId(${id})">Editar</a>
+                      <button class="btn btn-danger mb-2" onclick="eliminarNota(${id})">Eliminar</button>
+                  </div>
+              </div>
+          </div>
+        `;
+    });
+  }
+  let nota = document.getElementById("notas");
+  nota.innerHTML = template_index;
+})
+ */
 
-  fetch("http://localhost:5219/api/Notas")
-    .then((r) => r.json())
-    .then((data) => {
-      // Access the data after it's fetched
-      nota.innerHTML = ""; // Clear existing content before adding search results
-      let resultado = data.filter((item) => item.title.toLowerCase().includes(valor.toLowerCase()));
+function MostrarNotas(notas = Notas){
+  var contenedorNotas = document.getElementById("notas");
+  contenedorNotas.innerHTML = "";
 
-      for (let element of resultado) {
-        template_notas += `
-        <div class="col-md-3 col-sm-6 col-lg-3">
-            <div class="card">
-                <div class="card-header">
-                <h4 class="card-title text-uppercase fw-bold">${element.title}</h4>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">${element.content}</p>
-                </div>
-                <div class="card-footer">
-                    <p>${element.date}</p>
-                </div>
-                <div class="d-flex justify-content-center gap-2 mt-2">
-                    <a href="./Actualizar.html" class="btn btn-primary mb-2" onclick="saveNoteId(${id})">Editar</a>
-                    <button class="btn btn-danger mb-2" onclick="eliminarNota(${element.id})">Eliminar</button>
-                </div>
+  notas.forEach(nota => {
+    let container = document.createElement("div");
+    container.className = "col-md-4";
+    container.id = `nota-${nota.id}`;
+    container.innerHTML = `
+    
+        <div class="card">
+            <div class="card-header">
+            <h4 class="card-title text-uppercase fw-bold">${nota.title}</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text">${nota.content}</p>
+            </div>
+            <div class="card-footer">
+                <p>${nota.date}</p>
+            </div>
+            <div class="d-flex justify-content-center gap-2 mt-2">
+                <a href="./Actualizar.html" class="btn btn-primary mb-2" >Editar</a>
+                <button class="btn btn-danger mb-2" onclick="eliminarNota(${nota.id})">Eliminar</button>
             </div>
         </div>
-        `;
-      }
-      nota.innerHTML = template_notas; // Update element content with filtered notes
-    })
-    .catch((error) => {
-      console.error("Error fetching notes:", error);
-      // Handle errors appropriately, e.g., display an error message to the user
-    });
-} */
+  
+  `;
+  contenedorNotas.appendChild(container);
+  })
+}
+  
+
+function buscarNota(){
+  var buscar = document.getElementById("search").value;
+  buscar.toLowerCase();
+  var notas = Notas.filter(item => item.Title.ToLowerCase().includes(buscar) & item.Content.toLowerCase().includes(buscar));
+
+  MostrarNotas(notas);
+  console.log(notas)
+}
+
+
